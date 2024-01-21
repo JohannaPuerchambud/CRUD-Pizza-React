913fc65dd2e671b831f33912b78f9a7583c49e56
JohannaPuerchambud <jepuerchambudp@utn.edu.ec>
2024-01-21 01:20:29 -0500

    logo

--- src/components/showPizzas.js
+++ src/components/showPizzas.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import Modal from 'react-modal';
 import ReactPaginate from 'react-paginate';
 import Swal from 'sweetalert2';
-// Removed unused withReactContent import
+import logo from './imagenes/logo.jpg';
 import { showAlert } from '../functions';
 <link
   rel="stylesheet"
@@ -187,7 +187,9 @@ const ShowPizzas = () => {
     <>
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">Pizzería</a>
+          <a className="navbar-brand" href="#">Pizzería
+          <img src={logo} alt="Logo" style={{ width: '100px', height: 'auto' }} />
+          </a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
