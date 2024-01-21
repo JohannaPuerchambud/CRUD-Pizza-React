29410fb0c99b83de2d7a0fcbd56686ba8f2422fa
JohannaPuerchambud <jepuerchambudp@utn.edu.ec>
2024-01-20 23:52:58 -0500

    a

--- src/components/showPizzas.js
+++ src/components/showPizzas.js
@@ -6,6 +6,7 @@ import Swal from 'sweetalert2';
 import { showAlert } from '../functions';
 
 
+
 <link
   rel="stylesheet"
   href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
